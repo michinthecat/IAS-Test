a2d14f9600cc7661e5199358c9f388894a577c9e
Jhonny Bravo - Dev <jebravo@bancolombia.com.co>
2024-06-01 11:30:09 -0500

    new endpoints avaible

--- src/main/java/ias/eventmanager/event/infraestructure/entrypoint/EventController.java
+++ src/main/java/ias/eventmanager/event/infraestructure/entrypoint/EventController.java
@@ -2,23 +2,37 @@ package ias.eventmanager.event.infraestructure.entrypoint;
 
 import ias.eventmanager.event.domain.model.EventModel;
 import ias.eventmanager.event.domain.usecase.EventUseCase;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping
 @RequiredArgsConstructor
+@Validated
 public class EventController {
 
     private final EventUseCase eventUseCase;
 
-    @GetMapping("events")
+    @GetMapping("/events")
     public Flux<EventModel> listEvents()
     {
         return eventUseCase.findAll();
+    }
+
+    @PostMapping("/events")
+    public Mono<EventModel> save(@Valid @RequestBody EventModel eventModel)
+    {
+        return eventUseCase.save(eventModel);
+    }
+
+    @GetMapping("/events/{id}")
+    public Mono<EventModel> findById(@PathVariable String id)
+    {
+        return eventUseCase.findById(id);
     }
 
 }
--- src/main/java/ias/eventmanager/event/utils/exception/EventRestControllerAdvice.java
+++ src/main/java/ias/eventmanager/event/utils/exception/EventRestControllerAdvice.java
@@ -4,4 +4,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class EventRestControllerAdvice {
+
+
 }
